291e5fc7a6b0a783bc65af81bfba247c55104ce9
ManojKumarMarumudi <84531888+ManojKumarMarumudi@users.noreply.github.com>
2023-11-14 16:29:09 +0530

    Create MergeSort.java

---
+++ Sortings/MergeSort.java
@@ -0,0 +1,53 @@
+public class MergeSort {
+	public static void mergeSort(int[] arr,int l,int r) {
+		if(l<r) {
+			int mid = (l+r)/2;
+			mergeSort(arr,l,mid);
+			mergeSort(arr,mid+1,r);
+			merge(arr,l,mid,r);
+		}
+	}
+	public static void merge(int[] arr, int l, int mid, int r) {
+		// TODO Auto-generated method stub
+		int n1=mid-l+1;
+		int n2=r-mid;
+		int lArr[] =new int[n1];
+		int rArr[] =new int[n2];
+		for(int x=0;x<n1;x++) {
+			lArr[x]=arr[l+x];
+		}
+		for(int x=0;x<n2;x++) {
+			rArr[x]=arr[mid+1+x];
+		}
+		int i=0,j=0,k=l;
+		while(i<n1 &&j<n2) {
+			if(lArr[i]<=rArr[j]) {
+				arr[k]=lArr[i];
+				i++;
+			}else {
+				arr[k]=rArr[j];
+				j++;
+			}
+			k++;
+		}
+		while(i<n1) {
+			arr[k]=lArr[i];
+			i++;
+			k++;
+		}
+		while(j<n2) {
+			arr[k]=rArr[j];
+			j++;
+			k++;
+		}	
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[]= {3,6,4,8,2,9,1};
+		mergeSort(arr,0,arr.length-1);
+		for(int i:arr) {
+			System.out.print(i+" ");
+		}
+	}
+
+}
